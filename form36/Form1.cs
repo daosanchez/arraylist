a84eda30ef011efd7b050ac168e4d6a5db64697c
sanchez <145916188+ravzasanchez@users.noreply.github.com>
2023-12-04 09:59:21 +0300

    Add files via upload

---
+++ form36/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form36
+{
+    public partial class Form1 : Form
+    {
+        ArrayList sehirler = new ArrayList();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sehirler.Add(txtSehirAdı.Text);
+
+        }
+    }
+}
